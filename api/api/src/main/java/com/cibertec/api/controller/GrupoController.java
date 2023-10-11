c0765796bf9ca065a56a2719290bb25a85570c78
sadohu <sadohu26@yahoo.com>
2023-10-11 17:59:57 -0500

    Add: SaveGrupo

--- api/api/src/main/java/com/cibertec/api/controller/GrupoController.java
+++ api/api/src/main/java/com/cibertec/api/controller/GrupoController.java
@@ -2,12 +2,15 @@ package com.cibertec.api.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.cibertec.api.model.Grupo;
 import com.cibertec.api.model.GrupoPrestamista;
@@ -19,13 +22,14 @@ import com.cibertec.api.service.PrestamistaMService;
 import lombok.AllArgsConstructor;
 
 @Controller
+@RequestMapping(value = "grupo")
 @AllArgsConstructor
 public class GrupoController {
     private GrupoService grupoService;
     private GrupoPrestamistaService grupoPrestamistaService;
     private PrestamistaMService prestamistaMService;
     
-    @GetMapping("listarGrupo")
+    @GetMapping({"/listar", "", "/"})
     public String listGrupo(Model model){
         PrestamistaM prestamistaM = prestamistaMService.getPrestamistaById(2).orElse(null);
         List<Grupo> listGrupo = new ArrayList<Grupo>();
@@ -36,18 +40,33 @@ public class GrupoController {
             }
         }
 
-        // List<GrupoPrestamista> xList = prestamistaMService.listarPrestamistaPorId(prestamistaM.getIdPrestamista()).getListPrestamista();
-
-        // List<Grupo> listGrupo = new ArrayList<Grupo>();
-
-        // for (GrupoPrestamista item : xList) {
-        //     System.out.println(item.getIdGrupo());
-        //     // grupoService.getGrupoById();
-        // }
-
-        // List<Grupo> listGrupo = grupoService.getGrupoList();
         model.addAttribute("list", listGrupo);
-        return "listGrupo";
+        return "GrupoListar";
+    }
+
+    @GetMapping("registrar")
+    public String addGrupo(Model model){
+        Grupo grupo = new Grupo();
+        model.addAttribute("grupo", grupo);
+        return "GrupoRegistrar";
+    }
+
+    @PostMapping("registrar")
+    public String addGrupo(@ModelAttribute("grupo") Grupo grupo, BindingResult result, Model model, SessionStatus status){
+        if(result.hasErrors()){
+            model.addAttribute("error", grupo);
+            return "GrupoRegistrar";
+        }
+        Grupo newGrupo = grupoService.saveGrupo(grupo);
+
+        PrestamistaM prestamistaM = prestamistaMService.getPrestamistaById(2).orElse(null);
+        List<Grupo> grupos = prestamistaM.getGrupos();
+        grupos.add(newGrupo);
+        prestamistaM.setGrupos(grupos);
+        prestamistaMService.guardarPrestamista(prestamistaM);
+
+        status.setComplete();
+        return "redirect:/grupo";
     }
 
 
