9a15b1c780a09c7a4f63d12f258d926aa03f89fd
jean pierre <jean23.32naej@gmail.com>
2023-10-14 14:59:05 -0500

    Rama Actualizada
    Limpia y actualizada

--- api/api/src/test/java/com/cibertec/api/ApiApplicationTests.java
+++ api/api/src/test/java/com/cibertec/api/ApiApplicationTests.java
@@ -1,26 +1,13 @@
 package com.cibertec.api;
 
-import java.sql.Date;
-import java.util.Objects;
-
-import javax.swing.JOptionPane;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.cibertec.api.model.PrestamistaM;
-import com.cibertec.api.service.PrestamistaMService;
-import com.cibertec.api.serviceImpl.PrestamistaMServiceImpl;
-
 @SpringBootTest
-class ApiApplicationTests extends PrestamistaMServiceImpl {
-	
-	@Autowired
-	PrestamistaMService prestamistaService;
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
