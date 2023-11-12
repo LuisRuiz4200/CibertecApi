8da9e30c79187ca8fb5cc0dc797561a97f417539
sadohu <sadohu26@yahoo.com>
2023-11-12 03:12:30 -0500

    [Fix] Warnings

--- api/api/src/main/java/com/cibertec/api/model/CuotaPrestamoPK.java
+++ api/api/src/main/java/com/cibertec/api/model/CuotaPrestamoPK.java
@@ -2,14 +2,13 @@ package com.cibertec.api.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Embeddable
 @Data
 public class CuotaPrestamoPK {
-	@Column(name="idprestamo")
+	@Column(name="idPrestamo")
 	private int idPrestamo;
-	@Column(name="idcuotaprestamo")
+	@Column(name="idCuotaprestamo")
 	private int idCuotaPrestamo;
 }
