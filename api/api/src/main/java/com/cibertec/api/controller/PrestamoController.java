0433e139fa7488a674bcc3d783ef8dcd4a5db0ec
sadohu <sadohu26@yahoo.com>
2023-11-25 11:17:31 -0500

    [Add] Prepare PrestamoDto for view

--- api/api/src/main/java/com/cibertec/api/controller/PrestamoController.java
+++ api/api/src/main/java/com/cibertec/api/controller/PrestamoController.java
@@ -12,13 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cibertec.api.model.CuotaPrestamo;
 import com.cibertec.api.model.GrupoPrestamista;
 import com.cibertec.api.model.Prestamista;
 import com.cibertec.api.model.Prestamo;
+import com.cibertec.api.model.PrestamoDto;
 import com.cibertec.api.model.Prestatario;
 import com.cibertec.api.model.SolicitudPrestamo;
 import com.cibertec.api.model.Usuario;
 import com.cibertec.api.reuzable.Utils;
+import com.cibertec.api.service.CuotaPrestamoService;
 import com.cibertec.api.service.GrupoPrestamistaService;
 import com.cibertec.api.service.PrestamistaService;
 import com.cibertec.api.service.PrestamoService;
@@ -38,6 +41,7 @@ public class PrestamoController {
 	PrestamistaService prestamistaService;
 	GrupoPrestamistaService grupoService;
 	PrestatarioService prestatarioService;
+	CuotaPrestamoService cuotaService;
 
 	@GetMapping({ "", "/", "listar" })
 	private String listar(Model model, HttpSession session) {
@@ -46,50 +50,26 @@ public class PrestamoController {
 			return "redirect:/login";
 
 		int rol = userLogged.getRol().getIdRol();
-		List<Prestamo> listaPrestamo = new ArrayList<Prestamo>();
+		List<PrestamoDto> listaPrestamo = new ArrayList<PrestamoDto>();
 
 		if (rol == Utils.ROL_ADMINISTRADOR)
-			listaPrestamo = prestamoService.listar();
+			// listaPrestamo = prestamoService.listar();
 
-		if (rol == Utils.ROL_JEFE_PRESTAMISTA) {
-			Prestamista jefePrestamista = prestamistaService.getPrestamistaById(userLogged.getPersona().getIdPersona())
-					.orElseThrow();
+			if (rol == Utils.ROL_JEFE_PRESTAMISTA) {
+				Prestamista jefePrestamista = prestamistaService
+						.getPrestamistaById(userLogged.getPersona().getIdPersona())
+						.orElseThrow();
 
-			// List<GrupoPrestamista> grupoAsignado =
-			// grupoService.listByJefe(jefePrestamista);
-			// print(grupoAsignado);
-
-			// List<Prestamista> asesores = grupoAsignado.stream().map(item ->
-			// item.getAsesorPrestamista())
-			// .collect(Collectors.toList());
-			// print(asesores);
-
-			// List<Prestatario> pretatarios = asesores.stream().flatMap(asesor ->
-			// asesor.getPrestatariosList().stream())
-			// .collect(Collectors.toList());
-			// print(pretatarios);
-
-			// List<SolicitudPrestamo> solicitudes = pretatarios.stream()
-			// .flatMap(prestatario -> prestatario.getListaSolicitudPrestamo().stream()
-			// .filter(solicitud -> "Aprobado".equals(solicitud.getEstado())))
-			// .collect(Collectors.toList());
-			// print(solicitudes);
-
-			// List<Prestamo> prestamos = solicitudes.stream().map(solicitud ->
-			// prestamoService.getBySolicitud(solicitud))
-			// .filter(Objects::nonNull)
-			// .collect(Collectors.toList());
-
-			List<Prestamo> prestamos = grupoService.listByJefe(jefePrestamista).stream()
-					.map(GrupoPrestamista::getAsesorPrestamista)
-					.flatMap(asesor -> asesor.getPrestatariosList().stream()
-							.flatMap(prestatario -> prestatario.getListaSolicitudPrestamo().stream()
-									.filter(solicitud -> "Aprobado".equalsIgnoreCase(solicitud.getEstado()))
-									.map(solicitud -> prestamoService.getBySolicitud(solicitud))
-									.filter(Objects::nonNull)))
-					.collect(Collectors.toList());
-			listaPrestamo = prestamos;
-		}
+				List<Prestamo> prestamos = grupoService.listByJefe(jefePrestamista).stream()
+						.map(GrupoPrestamista::getAsesorPrestamista)
+						.flatMap(asesor -> asesor.getPrestatariosList().stream()
+								.flatMap(prestatario -> prestatario.getListaSolicitudPrestamo().stream()
+										.filter(solicitud -> "Aprobado".equalsIgnoreCase(solicitud.getEstado()))
+										.map(solicitud -> prestamoService.getBySolicitud(solicitud))
+										.filter(Objects::nonNull)))
+						.collect(Collectors.toList());
+				// listaPrestamo = prestamos;
+			}
 
 		if (rol == Utils.ROL_PRESTAMISTA) {
 			Prestamista asesorPrestamista = prestamistaService
@@ -101,7 +81,7 @@ public class PrestamoController {
 							.map(solicitud -> prestamoService.getBySolicitud(solicitud))
 							.filter(Objects::nonNull))
 					.collect(Collectors.toList());
-			listaPrestamo = prestamos;
+			// listaPrestamo = prestamos;
 		}
 
 		if (rol == Utils.ROL_PRESTATARIO) {
@@ -112,11 +92,13 @@ public class PrestamoController {
 					.map(solicitud -> prestamoService.getBySolicitud(solicitud))
 					.filter(Objects::nonNull)
 					.collect(Collectors.toList());
-			listaPrestamo = prestamos;
+			// listaPrestamo = prestamos;
+
+			listaPrestamo = prestamosToDto(prestamos);
 		}
 
 		if (listaPrestamo.size() > 0)
-			listaPrestamo.sort(Comparator.comparingInt(Prestamo::getIdPrestamo).reversed());
+			listaPrestamo.sort(Comparator.comparingInt(PrestamoDto::getIdPrestamo).reversed());
 
 		model.addAttribute("listaPrestamo", listaPrestamo);
 
@@ -141,6 +123,61 @@ public class PrestamoController {
 		model.addAttribute("prestamo", prestamo);
 
 		return "guardarPrestamo";
+	}
+
+	private List<PrestamoDto> prestamosToDto(List<Prestamo> prestamos) {
+		List<PrestamoDto> prestamosDto = new ArrayList<PrestamoDto>();
+
+		for (Prestamo prestamo : prestamos) {
+			PrestamoDto itemDto = new PrestamoDto();
+			List<CuotaPrestamo> cuotas = cuotaService.listarPorId(prestamo.getIdPrestamo());
+
+			double montosPagados = 0;
+			double montosPendientes = 0;
+			int cantCuotasPagadas = 0;
+			int cantCuotasPendientes = 0;
+			int nroCuotaActual = 0;
+
+			for (CuotaPrestamo cuota : cuotas) {
+				if ("Cancelado".equals(cuota.getEstado())) {
+					montosPagados += cuota.getMontoTotal();
+					cantCuotasPagadas++;
+				} else {
+					montosPendientes += cuota.getMontoTotal();
+					cantCuotasPendientes++;
+				}
+			}
+
+			if (cantCuotasPagadas == prestamo.getCuotas()) {
+				nroCuotaActual = 0;
+				itemDto.setEstadoPrestamo("Cancelado");
+				itemDto.setEstadoUltimaCuota("Cancelado");
+			} else {
+				final int finalNroCuotaActual = cantCuotasPagadas + 1;
+				itemDto.setEstadoPrestamo("Pendiente");
+				itemDto.setEstadoUltimaCuota(cuotas.stream()
+						.filter(cuota -> cuota.getCuotaPrestamoPk().getIdCuotaPrestamo() == finalNroCuotaActual)
+						.findFirst().orElseThrow().getEstado());
+			}
+
+			itemDto.setIdPrestamo(prestamo.getIdPrestamo());
+			itemDto.setSolicitudPrestamo(prestamo.getSolicitudPrestamo());
+			itemDto.setMonto(prestamo.getMonto());
+			itemDto.setCuotas(prestamo.getCuotas());
+			itemDto.setTea(prestamo.getTea());
+			itemDto.setTem(prestamo.getTem());
+			itemDto.setFechaRegistro(prestamo.getFechaRegistro());
+			itemDto.setActivo(prestamo.isActivo());
+			itemDto.setNroCuotaActual(nroCuotaActual);
+			itemDto.setCantCuotasPagadas(cantCuotasPagadas);
+			itemDto.setCantCuotasPendientes(cantCuotasPendientes);
+			itemDto.setMontosPagados(montosPagados);
+			itemDto.setMontosPendientes(montosPendientes);
+			itemDto.setListaCuotaPrestamo(cuotas);
+
+			prestamosDto.add(itemDto);
+		}
+		return prestamosDto;
 	}
 
 }
