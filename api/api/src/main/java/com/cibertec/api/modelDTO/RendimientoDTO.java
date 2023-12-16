9da50c22d2dd5706876d11b36a8e3a636a2b4e50
sadohu <sadohu26@yahoo.com>
2023-12-16 17:39:16 -0500

    Add RendimientoDTO class

---
+++ api/api/src/main/java/com/cibertec/api/modelDTO/RendimientoDTO.java
@@ -0,0 +1,20 @@
+package com.cibertec.api.modelDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class RendimientoDTO {
+    private int idJefePrestamista;
+    private int idPrestamista;
+    private double totalPrestado;
+    private double totalInteres;
+    private double totalPagado;
+    private double totalPendiente;
+    private double rentabilidad;
+}
